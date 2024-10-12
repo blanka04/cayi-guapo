4f1189e895cf0de21ea6f8f558afc4fdce2f9519
blanka04 <blancacampe13@hotmail.com>
2024-10-12 02:26:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let noClickCount = 0;
+
+document.getElementById('no').addEventListener('click', function() {
+  const noButton = document.getElementById('no');
+  const message = document.getElementById('message');
+
+  noClickCount++;
+  noButton.style.transform = 'scale(0.8)'; // Hacer el botón más pequeño
+
+  if (noClickCount === 1) {
+    message.textContent = '¿Seguro?';
+  } else if (noClickCount === 2) {
+    message.textContent = '¿Estás seguro cieguito?';
+  } else if (noClickCount === 3) {
+    message.textContent = 'Dale al sí, bobo';
+    noClickCount = 0; // Reiniciar el contador
+  }
+});
+
+document.getElementById('yes').addEventListener('click', function() {
+  const message = document.getElementById('message');
+  const imageContainer = document.getElementById('imageContainer');
+
+  message.textContent = 'Sii, muy bien cayi uwu';
+  
+  // Mostrar una imagen al final
+  imageContainer.innerHTML = '<img src="Picsart_24-10-12_02-03-11-962.jpg" alt="Imagen final" width="300">';
+});
